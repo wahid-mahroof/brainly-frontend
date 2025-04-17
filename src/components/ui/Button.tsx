fe0a9226264085a87b470c9d4793be254dceb88e
wahid-mahroof <wahidmahroof040@gmail.com>
2025-04-17 10:51:53 -0700

    frontend start

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,13 @@
+
+export interface ButtonProps {
+  variant:"primary" | "secondary"
+  size:"sm"| "md" | "lg";
+  text:string;
+  startIcon:
+  any;
+}
+export const Button = () =>{
+return <button></button>
+}
+
+<Button variant="primary" size="md" onClick={} text={}
